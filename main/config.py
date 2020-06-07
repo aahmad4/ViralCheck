5faca65b461ce8536d5190dc7e5505061922b8d4
Ali Ahmad <alislearning123@gmail.com>
2020-06-07 17:33:12 -0400

    Update config.py

--- main/config.py
+++ main/config.py
@@ -6,5 +6,5 @@ class Config:
   MAIL_SERVER = 'smtp.googlemail.com'
   MAIL_PORT = 587
   MAIL_USE_TLS = True
-  MAIL_USERNAME = 'viralcheck123@gmail.com'
-  MAIL_PASSWORD = 'Viralcheck123!'
+  MAIL_USERNAME = ''
+  MAIL_PASSWORD = ''
